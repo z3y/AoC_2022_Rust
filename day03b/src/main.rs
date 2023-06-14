2f77abef4f13d2009f0b472d4147f45bcc14fb81
z3y <33181641+z3y@users.noreply.github.com>
2023-06-14 18:02:58 +0200

    day03b

---
+++ day03b/src/main.rs
@@ -0,0 +1,31 @@
+use std::{fs::File, io::{BufReader, BufRead}};
+fn main()  {
+    let file = File::open("input.txt").unwrap();
+    let mut lines = BufReader::new(file).lines();
+
+    let mut score = 0;
+    loop {
+        match lines.next() {
+            Some(first) => {
+                let first = first.unwrap();
+                let second = lines.next().unwrap().unwrap();
+                let third = lines.next().unwrap().unwrap();
+
+                let shared = first.chars().into_iter().find(|a| {
+                        second.chars().into_iter().find(|b| {
+                            third.chars().into_iter().find(|c| c == b && c == a).is_some()
+                        }).is_some()
+                    }).unwrap();
+                score += match shared.is_uppercase() {
+                    true => shared as u32 - 'A' as u32 + 27,
+                    false => shared as u32 - 'a' as u32 + 1,
+                }
+
+            },
+            None => { break }
+        }
+    }
+
+    println!("{}", score);
+
+}
