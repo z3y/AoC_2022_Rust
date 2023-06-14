bd13e0e99d3aff3117519b30204082d3ee306170
z3y <33181641+z3y@users.noreply.github.com>
2023-06-14 18:54:30 +0200

    make this cleaner

--- day03b/src/main.rs
+++ day03b/src/main.rs
@@ -4,25 +4,19 @@ fn main()  {
     let mut lines = BufReader::new(file).lines();
 
     let mut score = 0;
-    loop {
-        match lines.next() {
-            Some(first) => {
-                let first = first.unwrap();
-                let second = lines.next().unwrap().unwrap();
-                let third = lines.next().unwrap().unwrap();
+    while let Some(first) = lines.next() {
+        let first = first.unwrap();
+        let second = lines.next().unwrap().unwrap();
+        let third = lines.next().unwrap().unwrap();
 
-                let shared = first.chars().into_iter().find(|a| {
-                        second.chars().into_iter().find(|b| {
-                            third.chars().into_iter().find(|c| c == b && c == a).is_some()
-                        }).is_some()
-                    }).unwrap();
-                score += match shared.is_uppercase() {
-                    true => shared as u32 - 'A' as u32 + 27,
-                    false => shared as u32 - 'a' as u32 + 1,
-                }
-
-            },
-            None => { break }
+        let shared = first.chars().into_iter().find(|a| {
+                second.chars().into_iter().find(|b| {
+                    third.chars().into_iter().find(|c| c == b && c == a).is_some()
+                }).is_some()
+            }).unwrap();
+        score += match shared.is_uppercase() {
+            true => shared as u32 - 'A' as u32 + 27,
+            false => shared as u32 - 'a' as u32 + 1,
         }
     }
 
