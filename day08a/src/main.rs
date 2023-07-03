acdb760d5c8ee960fcf9ad44595feb982c529c62
z3y <33181641+z3y@users.noreply.github.com>
2023-07-03 15:56:11 +0200

    condense into loop

--- day08a/src/main.rs
+++ day08a/src/main.rs
@@ -12,13 +12,24 @@ enum MoveDirection {
     Left,
     Right
 }
+impl MoveDirection {
+    fn from_int(value: i32) -> Self {
+        match value {
+            0 => MoveDirection::Up,
+            1 => MoveDirection::Down,
+            2 => MoveDirection::Left,
+            3 => MoveDirection::Right,
+            _ => panic!(),
+        }
+    }
+}
 impl Talbe {
         
     fn check_bounds(&self) -> bool {
         self.x >= 0 && self.x < self.size && self.y >= 0 && self.y < self.size
     }
 
-    fn move_cell(&mut self, direction: MoveDirection) -> Option<i32> {
+    fn move_cell(&mut self, direction: &MoveDirection) -> Option<i32> {
         match direction {
             MoveDirection::Up => self.x -= 1,
             MoveDirection::Down => self.x += 1,
@@ -54,77 +65,28 @@ fn main() {
         for y in 0..table.size {
 
             let mut visible = true;
-            table.x = x;
-            table.y = y;
-            let current_tree_height = table.value_at_position();
-            loop {
-                match table.move_cell(MoveDirection::Up) {
-                    Some(tree) => {
-                        if tree >= current_tree_height {
-                            visible = false;
-                            break;
-                        }
-                    },
-                    None => break
+            for dir in 0..=3 {
+                visible = true;
+                let dir = MoveDirection::from_int(dir);
+                table.x = x;
+                table.y = y;
+                let current_tree_height = table.value_at_position();
+                loop {
+                    match table.move_cell(&dir) {
+                        Some(tree) => {
+                            if tree >= current_tree_height {
+                                visible = false;
+                                break;
+                            }
+                        },
+                        None => break
+                    }
                 }
-            }
-            if visible {
-                visible_trees_count +=1;
-                continue;
+                if visible {
+                    break;
+                }
             }
 
-            visible = true;
-            table.x = x;
-            table.y = y;
-            loop {
-                match table.move_cell(MoveDirection::Down) {
-                    Some(tree) => {
-                        if tree >= current_tree_height {
-                            visible = false;
-                            break;
-                        }
-                    },
-                    None => break
-                }
-            }
-            if visible {
-                visible_trees_count +=1;
-                continue;
-            }
-
-            visible = true;
-            table.x = x;
-            table.y = y;
-            loop {
-                match table.move_cell(MoveDirection::Left) {
-                    Some(tree) => {
-                        if tree >= current_tree_height {
-                            visible = false;
-                            break;
-                        }
-                    },
-                    None => break
-                }
-            }
-            if visible {
-                visible_trees_count +=1;
-                continue;
-            }
-
-            visible = true;
-            table.x = x;
-            table.y = y;
-            loop {
-                match table.move_cell(MoveDirection::Right) {
-                    Some(tree) => {
-                        if tree >= current_tree_height {
-                            visible = false;
-                            break;
-                        }
-                    },
-                    None => break
-                }
-            }
             if visible {
                 visible_trees_count +=1;
                 continue;
